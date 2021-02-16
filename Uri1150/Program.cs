b415f7f43048b0d156fa6e580eacae380e80a51c
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 16:33:17 -0300

    Add files via upload

---
+++ Uri1150/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Uri1150
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, z, c, soma;
+
+            x = int.Parse(Console.ReadLine());
+            z = int.Parse(Console.ReadLine());
+
+
+            while(z<=x)
+            {
+                z = int.Parse(Console.ReadLine());
+            }
+
+            c = 1;
+            soma = x;
+
+            do
+            {
+
+                x = x + 1;
+                soma = soma + x;
+                c = c + 1;
+
+            } while (soma <= z);
+
+            Console.WriteLine(soma);
+
+        }
+    }
+}
